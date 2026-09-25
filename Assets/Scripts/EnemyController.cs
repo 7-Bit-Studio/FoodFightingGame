fb39e1a51b8519914e58164b8576ef1503712ef7
rlcbon08 <rlcbon08@gmail.com>
2026-09-25 13:53:06 -0400

    Added taking damage; removed unused variables

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -22,23 +22,23 @@ public class EnemyController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float maxSpeed = 5f;
-    [SerializeField] private float enemyAcc = 25f;
-    [SerializeField] private float frictionCoefficient = 1.01f;
-    [SerializeField] private bool useAcceleration = true;
     [SerializeField] private SpriteAtlas spriteAtlas;
+    [SerializeField] private int health = 100;
+    [SerializeField] private int defense = 0;
+    [SerializeField] private float acceleration = 0.2f;
     /// <summary>
     /// How many seconds for each animation frame to last
     /// </summary>
     [SerializeField] private float animationDuration = 0.25f;
 
-    // Player values
+    // EnemyController values
     private Transform enemyPos;
     private Vector3 enemyVel;
 
-    // PlayerData Values
+    // EnemyData Values
     private VarTypes.Direction facing;
     private VarTypes.Data data;
-    private Movement movement;
+    private PlayerController movement;
 
     private Sprite[] spriteArr;
     private Sprite currentSprite;
@@ -50,33 +50,37 @@ public class EnemyController : MonoBehaviour
 
     private Vector3 playerDirection;
     private Vector3 normalizedPlayerDirection;
-    private Scene scene;
-    private GameObject[] gameObjects;
     private GameObject player;
+    private bool isStunned;
+    private bool wasJustHit;
+    private float hitTime;
+
 
     void Start()
     {
+        wasJustHit = false;
+        isStunned = false;
         player = Functions.GetSiblingGameObject(transform, "Player");
 
-        movement = player.GetComponent<Movement>();
+        movement = player.GetComponent<PlayerController>();
 
         data = movement.GetData();
 
         enemyPos = GetComponent<Transform>();
         data.velocity = enemyVel;
         data.position = enemyPos;
+        data.health = health;
 
         spriteArr = new Sprite[spriteAtlas.spriteCount];
         timeAtLastSpriteUpdate = Time.time;
         currentSpriteIndex = 0;
         spriteAtlas.GetSprites(spriteArr);
-
-        Debug.Log(spriteArr.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!transform.gameObject.activeSelf) return;
         data = movement.GetData();
         
         playerDirection = data.position.transform.position - transform.position;
@@ -95,9 +99,50 @@ public class EnemyController : MonoBehaviour
         UpdateMovement();
     }
 
+    public void OnHit(int damage)
+    {
+        int dealtDamage = damage - defense;
+        dealtDamage = dealtDamage < 0 ? 0 : dealtDamage;
+
+        health -= dealtDamage;
+
+        Debug.Log($"{transform.name} was hit for {damage} damage!");
+
+        enemyVel = -normalizedPlayerDirection * 3;
+        hitTime = Time.time;
+        wasJustHit = true;
+
+        if(health <= 0)
+        {
+            transform.gameObject.SetActive(false);
+        }
+    }
     void UpdateMovement()
     {
-        enemyVel = maxSpeed * playerDirection;
+        if (wasJustHit)
+        {
+            if (Time.time - hitTime > 0.25f)
+            {
+                wasJustHit = false;
+                isStunned = true;
+            }
+
+
+            UpdatePosition();
+            return;
+        }
+
+        if (isStunned)
+        {
+            if(Time.time - hitTime > 1f) isStunned = false;
+
+            enemyVel *= 0.95f;
+
+            UpdatePosition();
+            return;
+        }
+
+        enemyVel += acceleration * deltaTime * normalizedPlayerDirection;
 
         if (enemyVel.sqrMagnitude > maxSpeed * maxSpeed)
         {
@@ -143,9 +188,12 @@ public class EnemyController : MonoBehaviour
             float otherColliderRad = gameObject.GetComponent<CircleCollider2D>().radius;
             Vector3 otherColliderCenter = gameObject.transform.position + (Vector3)gameObject.GetComponent<CircleCollider2D>().offset;
 
-            if (Vector3.Distance(colliderCenter, otherColliderCenter) < otherColliderRad + colliderRad)
+            float currentDistance = Vector3.Distance(colliderCenter, otherColliderCenter);
+            float minDistance = colliderRad + otherColliderRad;
+
+            if (currentDistance < otherColliderRad + colliderRad)
             {
-                enemyPos.position += (colliderCenter - otherColliderCenter) * ((otherColliderRad + colliderRad) - Vector3.Distance(colliderCenter, otherColliderCenter));
+                enemyPos.position += (colliderCenter - otherColliderCenter) * (minDistance - currentDistance);
                 Debug.Log("Collision!");
             }
         }
